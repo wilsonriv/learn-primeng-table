e62fd42d077cceae58807bf10ee65fbb1af54f11
wilsonriv <sabdiel.rivera@gmail.com>
2022-01-14 16:05:27 -0500

    Dynamic Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,19 @@ export interface Car {
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   cars: Car[];
+  cols: any[];
 
   ngOnInit() {
     this.cars = [
       { vin: '12345', year: 2005, brand: 'Toyota', color: 'black' },
       { vin: '67891', year: 2006, brand: 'Hyunday', color: 'blue' },
+    ];
+
+    this.cols = [
+      { field: 'vin', header: 'Vin' },
+      { field: 'year', header: 'Year' },
+      { field: 'brand', header: 'Brand' },
+      { field: 'color', header: 'Color' },
     ];
   }
 }
